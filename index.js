76893b252621b4d9446f228b98a360b754ee4274
Mike93kl <mikeyyg9@gmail.com>
2020-02-26 18:28:27 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // require servers
 const {io} = require('./servers');
 const game = require('./game/Game');
+const settings  = require('./game/config/default_settings');
 
 io.sockets.on('connect', (socket) => {
     let player;
@@ -10,11 +11,54 @@ io.sockets.on('connect', (socket) => {
         socket.emit('init', {
             orbs: game.orbs,
             players: game.players,
-            playerX: player.location.x,
-            playerY: player.location.y
+            player_location: player.location
         });
+
+        // emit tock every 33ms
+        setInterval(() => {
+            socket.emit('tock', {
+                player_location: player.location,
+                players: game.players
+            });
+        }, 33)
+
+
+        socket.on('tick', (mouseData) => {
+            const xV = mouseData.x;
+            const yV = mouseData.y;
+            const speed = player.speed;
+            if((player.location.x < 5 && xV < 0) || ( player.location.x > settings.world_width && (xV > 0)) ) {
+                player.location.y -= speed * yV;
+            }else if((player.location.x < 5 && yV > 0) || ((player.location.y > settings.world_height) && (yV < 0)) ){
+                player.location.x += speed * xV;
+            }else {
+                player.location.x += speed * xV;
+                player.location.y -= speed * yV;
+            }
+
+            orbCollision(player,game.orbs)
+            .then((index) => {
+                player.radius += 1;
+                game.destroyOrb(index);
+                socket.emit('newOrb', {
+                    index, orb: game.orbs[index]
+                })
+            }).catch((e) => {})
+    
+        });
+
     });
 
-
-
 });
+
+function orbCollision(player, orbs) {
+    return new Promise((resolve,reject) => {
+        orbs.forEach((orb,i) => {
+            if(orb.collidedWith(player)) {
+                console.log('collision');
+                resolve(i);
+            }
+        });
+        reject();
+    });
+}
